c7eff3c81d6c033384d182b7eeb18590b0d491fb
Hyeonjun Park <phjppo0918@gmail.com>
2023-04-01 19:41:10 +0900

    feat : signup/login dto 구현

---
+++ kotlin-server/src/main/kotlin/com/example/kotlinserver/member/dto/SignUpRequest.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinserver.member.dto
+
+import com.example.kotlinserver.member.Member
+
+data class SignUpRequest(
+    val email: String,
+    val nickname: String,
+    val password: String
+) {
+    fun toEntity() : Member = Member(email = email, nickname = nickname, password = password)
+}
+
